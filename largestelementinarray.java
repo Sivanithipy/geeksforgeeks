577d928c7ac34d0df3d2c5d431e19a316b66bafd
Sivanithipy <pyshivanithi@gmail.com>
2025-03-02 16:20:43 +0530

    Largest Element in Array

---
+++ largestelementinarray.java
@@ -0,0 +1,13 @@
+class largestelementinarray {
+  public static int largest(int[] arr) {
+      int lar=arr[0];
+      for(int i=0;i<arr.length;i++)
+      {
+          if(arr[i]>lar)
+          {
+              lar=arr[i];
+          }
+      }
+      return lar;
+  }
+}
