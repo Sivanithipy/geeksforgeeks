c7a8721d2c57f69f2df6689a7f0567cb0fd6999c
Sivanithipy <pyshivanithi@gmail.com>
2025-03-05 16:00:52 +0530

    Move Zeros

---
+++ movezeros.java
@@ -0,0 +1,16 @@
+class movezeros  {
+  void pushZerosToEnd(int[] nums) {
+       int i=0;
+      for(final int num:nums)
+      {
+          if(num!=0)
+          {
+              nums[i++]=num;
+          }
+      }
+      while(i<nums.length)
+      {
+          nums[i++]=0;
+      }
+  }
+} 
