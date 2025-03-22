ad4053c2e9197688853c8dd2f62e1ab83dda2862
Sivanithipy <pyshivanithi@gmail.com>
2025-03-22 13:23:49 +0530

    Check if array is sorted

---
+++ arrissorted.java
@@ -0,0 +1,14 @@
+
+class arrissorted  {
+  public boolean arraySortedOrNot(int[] arr) {
+      for(int i=0;i<arr.length-1;i++)
+      {
+          if(arr[i]>arr[i+1])
+          {
+              return false;
+          }
+      }
+      return true;
+  }
+}
+ 
