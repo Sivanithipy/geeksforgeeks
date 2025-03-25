f01c3209c9ffdc29edf034325ae33254837ab4e3
Sivanithipy <pyshivanithi@gmail.com>
2025-03-25 18:54:43 +0530

    Reverse an Array

---
+++ reverseanarray.java
@@ -0,0 +1,16 @@
+class reverseanarray {
+  public void reverseArray(int arr[]) {
+      int l=0;
+      int r=arr.length-1;
+     while(l<r)
+     {
+         int temp=arr[l];
+         arr[l]=arr[r];
+         arr[r]=temp;
+         l++;
+         r--;
+     }
+     
+  }
+}
+ 
