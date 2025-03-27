653f0859d277becf88634f5dc7a2aec530dc1a8b
Sivanithipy <pyshivanithi@gmail.com>
2025-03-27 10:35:38 +0530

    Search in Linked List

---
+++ searchinll.java
@@ -0,0 +1,14 @@
+class searchinll  {
+  static boolean searchKey(int n, Node head, int key) {
+          Node temp=head;
+          while(temp!=null)
+          {
+              if(temp.data==key)
+              {
+                  return true;
+              }
+              temp=temp.next;
+          }
+      return false;
+  }
+}
