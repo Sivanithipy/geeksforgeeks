3386513555b1ff2e2bed08446c4bee53021bfd01
Sivanithipy <pyshivanithi@gmail.com>
2025-04-30 11:16:36 +0530

    Reverse a String

---
+++ revastring.java
@@ -0,0 +1,10 @@
+class revastring {
+    public static String reverseString(String s) {
+        StringBuilder str = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) 
+        {
+            str.append(s.charAt(i));
+        }
+        return str.toString();
+    }
+}
